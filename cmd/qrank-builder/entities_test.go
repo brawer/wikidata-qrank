956d0889733b10097897be3a3b2484d3e17922a3
Sascha Brawer <sascha@brawer.ch>
2021-02-23 10:14:59 +0100

    Add a benchmark for ProcessEntity()

--- cmd/qrank-builder/entities_test.go
+++ cmd/qrank-builder/entities_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"os"
 	"path/filepath"
 	"testing"
+
+	"github.com/dsnet/compress/bzip2"
 )
 
 func TestFindEntitiesDump(t *testing.T) {
@@ -45,4 +49,60 @@ func TestFindEntitiesDump(t *testing.T) {
 	if !os.SameFile(expected, got) {
 		t.Errorf("expected %q, got %q", expectedPath, path)
 	}
+}
+
+func BenchmarkProcessEntity(b *testing.B) {
+	data, err := readTestEntities("testdata/twenty_entities.json.bz2")
+	if err != nil {
+		b.Error(err)
+		return
+	}
+
+	ch := make(chan string, 1000)
+	defer close(ch)
+	go func() {
+		for _ = range ch {
+		}
+	}()
+
+	ctx := context.Background()
+	for i := 0; i < b.N; i++ {
+		if err := processEntity(data[i%len(data)], ch, ctx); err != nil {
+			b.Error(err)
+			return
+		}
+	}
+}
+
+func readTestEntities(path string) ([][]byte, error) {
+	data := make([][]byte, 0, 20)
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader, err := bzip2.NewReader(file, &bzip2.ReaderConfig{})
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+
+	scanner := bufio.NewScanner(reader)
+	maxLineSize := 1 * 1024 * 1024
+	scanner.Buffer(make([]byte, maxLineSize), maxLineSize)
+	for scanner.Scan() {
+		buf := scanner.Bytes()
+		if len(buf) > 3 { // leading '[', trailing ']' line in file
+			if buf[len(buf)-1] == ',' {
+				buf = buf[0 : len(buf)-1]
+			}
+			data = append(data, buf)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return data, nil
 }
