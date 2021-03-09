4410032627ca2e3604fc6aa4275293da18371f27
Sascha Brawer <sascha@brawer.ch>
2021-03-09 08:49:15 +0100

    Add LineMerger to merge pre-sorted files

---
+++ cmd/qrank-builder/linemerger.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"bufio"
+	"container/heap"
+	"io"
+)
+
+// Merges the lines of a multiple io.Readers whose content is in sorted order.
+type LineMerger struct {
+	heap   lineMergerHeap
+	err    error
+	inited bool
+}
+
+func NewLineMerger(r []io.Reader) *LineMerger {
+	m := &LineMerger{}
+	m.heap = make(lineMergerHeap, 0, len(r))
+	for _, rr := range r {
+		item := &mergee{scanner: bufio.NewScanner(rr)}
+		if item.scanner.Scan() {
+			item.line = item.scanner.Text()
+			m.heap = append(m.heap, item)
+		}
+		if err := item.scanner.Err(); err != nil {
+			m.err = err
+			return m
+		}
+	}
+	return m
+}
+
+func (m *LineMerger) Advance() bool {
+	if m.err != nil {
+		return false
+	}
+	if len(m.heap) == 0 {
+		return false
+	}
+	if !m.inited {
+		heap.Init(&m.heap)
+		m.inited = true
+		return true
+	}
+	item := m.heap[0]
+	if item.scanner.Scan() {
+		item.line = item.scanner.Text()
+		heap.Fix(&m.heap, 0)
+	} else {
+		heap.Remove(&m.heap, 0)
+	}
+	if err := item.scanner.Err(); err != nil {
+		m.err = err
+		return false
+	}
+	return len(m.heap) > 0
+}
+
+func (m *LineMerger) Err() error {
+	return m.err
+}
+
+func (m *LineMerger) Line() string {
+	n := len(m.heap)
+	if n > 0 {
+		return m.heap[0].line
+	} else {
+		return ""
+	}
+}
+
+type mergee struct {
+	line    string
+	scanner *bufio.Scanner
+	index   int
+}
+
+type lineMergerHeap []*mergee
+
+func (h lineMergerHeap) Len() int { return len(h) }
+
+func (h lineMergerHeap) Less(i, j int) bool {
+	return h[i].line < h[j].line
+}
+
+func (h lineMergerHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].index = i
+	h[j].index = j
+}
+
+func (h *lineMergerHeap) Push(x interface{}) {
+	n := len(*h)
+	item := x.(*mergee)
+	item.index = n
+	*h = append(*h, item)
+}
+
+func (h *lineMergerHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil // avoid memory leak
+	item.index = -1
+	*h = old[0 : n-1]
+	return item
+}
---
+++ cmd/qrank-builder/linemerger_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"strings"
+	"testing"
+)
+
+func TestLineMerger(t *testing.T) {
+	m := NewLineMerger([]io.Reader{
+		strings.NewReader("C1\nD1"),
+		strings.NewReader("B2\nE2"),
+		strings.NewReader("A3\nB3"),
+		strings.NewReader(""),
+		strings.NewReader("B5"),
+	})
+	result := make([]string, 0, 5)
+	for m.Advance() {
+		result = append(result, m.Line())
+	}
+	if err := m.Err(); err != nil {
+		t.Error(err)
+		return
+	}
+	got := strings.Join(result, "|")
+	expected := "A3|B2|B3|B5|C1|D1|E2"
+	if expected != got {
+		t.Errorf("expected %q, got %q", expected, got)
+	}
+}
+
+type errReader struct{}
+
+var testErr = errors.New("test error")
+
+func (e *errReader) Read(p []byte) (n int, err error) {
+	return 0, testErr
+}
+
+func TestLineMergerError(t *testing.T) {
+	m := NewLineMerger([]io.Reader{&errReader{}})
+	if m.Advance() {
+		t.Error("expected m.Advance()=false, got true")
+		return
+	}
+	if err := m.Err(); err != testErr {
+		t.Errorf("expected test error, got %q", err)
+	}
+}
