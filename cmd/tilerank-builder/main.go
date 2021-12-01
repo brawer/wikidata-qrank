b68fd5dcad28ba010f2c3cf9c6798ea562aebca0
Sascha Brawer <sascha@brawer.ch>
2021-12-01 07:45:24 +0100

    Refactor main(), document each step

--- cmd/tilerank-builder/main.go
+++ cmd/tilerank-builder/main.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -12,38 +13,69 @@ import (
 var logger *log.Logger
 
 func main() {
-	logpath := filepath.Join("logs", "tilerank-builder.log")
-	if err := os.MkdirAll("logs", os.ModePerm); err != nil {
-		log.Fatal(err)
-	}
-
-	logfile, err := os.OpenFile(logpath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	logfile, err := createLogFile()
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer logfile.Close()
 	logger = log.New(logfile, "", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
 
+	cachedir := "cache"
+	maxWeeks := 3 * 52 // 3 years
+	if _, err := fetchWeeklyLogs(cachedir, maxWeeks); err != nil {
+		logger.Fatal(err)
+	}
+}
+
+// Create a file for keeping logs. If the file already exists, its
+// present content is preserved, and new log entries will get appended
+// after the existing ones.
+func createLogFile() (*os.File, error) {
+	logpath := filepath.Join("logs", "tilerank-builder.log")
+	if err := os.MkdirAll("logs", os.ModePerm); err != nil {
+		return nil, err
+	}
+
+	logfile, err := os.OpenFile(logpath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	return logfile, nil
+}
+
+// Fetch log data for up to `maxWeeks` weeks from planet.openstreetmap.org.
+// For each week, the seven daily log files are fetched from OpenStreetMap,
+// and combined into a one single compressed file, stored on local disk.
+// If this weekly file already exists on disk, we return its content directly
+// without re-fetching that week from the server. Therefore, if this tool
+// is run periodically, it will only fetch the content that has not been
+// downloaded before. The result is an array of readers, one for each week.
+func fetchWeeklyLogs(cachedir string, maxWeeks int) ([]io.Reader, error) {
 	client := &http.Client{}
 	weeks, err := GetAvailableWeeks(client)
 	if err != nil {
-		logger.Fatal(err)
-		return
+		return nil, err
 	}
 
-	maxWeeks := 3 * 52 // 3 years
 	if len(weeks) > maxWeeks {
 		weeks = weeks[len(weeks)-maxWeeks:]
 	}
-	logger.Printf(
-		"found %d weeks with OpenStreetMap tile logs, from %s to %s",
-		len(weeks), weeks[0], weeks[len(weeks)-1])
 
-	cachedir := "cache"
+	if logger != nil {
+		logger.Printf(
+			"found %d weeks with OpenStreetMap tile logs, from %s to %s",
+			len(weeks), weeks[0], weeks[len(weeks)-1])
+	}
+
+	readers := make([]io.Reader, 0, len(weeks))
 	for _, week := range weeks {
-		if _, err := GetTileLogs(week, client, cachedir); err != nil {
-			logger.Fatal(err)
-			return
+		if r, err := GetTileLogs(week, client, cachedir); err == nil {
+			readers = append(readers, r)
+		} else {
+			return nil, err
 		}
 	}
+
+	return readers, nil
 }
