8a6428f6d47e510150c210fdc9715803ba1078e2
Sascha Brawer <sascha@brawer.ch>
2021-12-03 16:02:30 +0100

    Consolidate all tile-related code into tile.go

--- cmd/tilerank-builder/tile.go
+++ cmd/tilerank-builder/tile.go
@@ -3,6 +3,8 @@
 package main
 
 import (
+	"encoding/binary"
+	"github.com/lanrat/extsort"
 	"math"
 )
 
@@ -49,4 +51,49 @@ func (t TileKey) ZoomXY() (zoom uint8, x, y uint32) {
 		shift += 2
 	}
 	return zoom, x, y
+}
+
+// TileCount counts the number of impressions for a tile.
+type TileCount struct {
+	Key   TileKey
+	Count uint64
+}
+
+// ToBytes serializes a TileCount into a byte array.
+func (c TileCount) ToBytes() []byte {
+	zoom, x, y := c.Key.ZoomXY()
+	buf := make([]byte, binary.MaxVarintLen32*2+binary.MaxVarintLen64+1)
+	pos := binary.PutUvarint(buf, uint64(x))
+	pos += binary.PutUvarint(buf[pos:], uint64(y))
+	pos += binary.PutUvarint(buf[pos:], c.Count)
+	buf[pos] = zoom
+	pos += 1
+	return buf[0:pos]
+}
+
+// TileCountFromBytes de-serializes a TileCount from a byte array.
+// The result is returned as an extsort.SortType because that is
+// needed by the library for external sorting.
+func TileCountFromBytes(b []byte) extsort.SortType {
+	x, pos := binary.Uvarint(b)
+	y, len := binary.Uvarint(b[pos:])
+	pos += len
+	count, len := binary.Uvarint(b[pos:])
+	pos += len
+	zoom := b[pos]
+	key := MakeTileKey(zoom, uint32(x), uint32(y))
+	return TileCount{Key: key, Count: count}
+}
+
+// TileCountLess returns true if TileCount a should be sorted before b.
+// The arguments are passed as extsort.SortType because that is
+// needed by the library for external sorting.
+func TileCountLess(a, b extsort.SortType) bool {
+	aa := a.(TileCount)
+	bb := b.(TileCount)
+	if aa.Key != bb.Key {
+		return aa.Key < bb.Key
+	} else {
+		return aa.Count < bb.Count
+	}
 }
