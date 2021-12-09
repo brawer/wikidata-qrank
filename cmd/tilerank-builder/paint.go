9388b4b0f23a09b9fa4e7c4ddc57a8fac0b3bb6c
Sascha Brawer <sascha@brawer.ch>
2021-12-09 18:12:20 +0100

    Compute view density for each raster pixel

--- cmd/tilerank-builder/paint.go
+++ cmd/tilerank-builder/paint.go
@@ -114,20 +114,29 @@ type Raster struct {
 }
 
 func (r *Raster) Paint(tile TileKey, viewsPerKm2 float32) {
-	rZoom, _, _ := r.tile.ZoomXY()
-	zoom := tile.Zoom()
+	rZoom, rX, rY := r.tile.ZoomXY()
 
 	// If the to-be-painted tile is smaller than 1 pixel, we scale it
 	// to one pixel and reduce the number of views accordingly.
 	// We only do this at deep zoom levels, where the area per pixel
 	// is nearly uniform despite the distortion of the web mercator
 	// projection.
-	if zoom > rZoom+8 {
-		viewsPerKm2 /= float32(int32(1 << (zoom - (rZoom + 8))))
+	if zoom := tile.Zoom(); zoom > rZoom+8 {
+		viewsPerKm2 /= float32(int32(1 << (2 * (zoom - (rZoom + 8)))))
 		tile = tile.ToZoom(rZoom + 8)
 	}
 
-	// TODO: Add viewsPerKm2 to those pixels that are touched by the tile.
+	zoom, x, y := tile.ZoomXY()
+	deltaZoom := zoom - rZoom
+	left := (x - rX<<deltaZoom) << (8 - deltaZoom)
+	top := (y - rY<<deltaZoom) << (8 - deltaZoom)
+	width := uint32(1 << (8 - deltaZoom))
+	// Because our tiles are squares, the height is the same as the width.
+	for y := top; y < top+width; y++ {
+		for x := left; x < left+width; x++ {
+			r.pixels[y<<8+x] += viewsPerKm2
+		}
+	}
 }
 
 func NewRaster(tile TileKey, parent *Raster) *Raster {
--- cmd/tilerank-builder/paint_test.go
+++ cmd/tilerank-builder/paint_test.go
@@ -5,6 +5,7 @@ package main
 import (
 	"context"
 	"io"
+	"math"
 	"os"
 	"path/filepath"
 	"strings"
@@ -47,5 +48,42 @@ func TestPaint_TooManyCountsForSameTile(t *testing.T) {
 	want := "tile 7/39/87 appears more than 1 times in input"
 	if got != want {
 		t.Fatalf("got %v, want %v", got, want)
+	}
+}
+
+func TestRaster_Paint(t *testing.T) {
+	r := NewRaster(MakeTileKey(1, 1, 1), NewRaster(WorldTile, nil))
+	r.Paint(MakeTileKey(2, 3, 3), 23)
+	r.Paint(MakeTileKey(3, 6, 7), 42)
+	wantPixels(t, r.pixels, [4][4]float32{
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 23, 23},
+		{0, 0, 65, 23},
+	})
+}
+
+func TestRaster_Paint_SubPixel(t *testing.T) {
+	tile := MakeTileKey(1, 0, 0)
+	r := NewRaster(tile, NewRaster(WorldTile, nil))
+	r.Paint(MakeTileKey(10, 256, 256), 100) // covers 1/4th of a pixel
+	wantPixels(t, r.pixels, [4][4]float32{
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 25, 0},
+		{0, 0, 0, 0},
+	})
+}
+
+func wantPixels(t *testing.T, got [256 * 256]float32, want [4][4]float32) {
+	px := []int{0, 64, 128, 192}
+	for j, vals := range want {
+		for i, wantPix := range vals {
+			x, y := px[i], px[j]
+			gotPix := got[y<<8+x]
+			if math.Abs(float64(gotPix-wantPix)) > 1e-4 {
+				t.Errorf("pixel (%d, %d): got %f, want %f", x, y, gotPix, wantPix)
+			}
+		}
 	}
 }
