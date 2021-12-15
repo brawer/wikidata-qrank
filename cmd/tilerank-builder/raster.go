a5916427dfa71e5c30a653365cb230d4b3a9665d
Sascha Brawer <sascha@brawer.ch>
2021-12-15 10:20:20 +0100

    Write TIFF output

--- cmd/tilerank-builder/raster.go
+++ cmd/tilerank-builder/raster.go
@@ -65,12 +65,21 @@ func NewRaster(tile TileKey, parent *Raster) *Raster {
 }
 
 type RasterWriter struct {
+	path             string
 	tiffTilesToSort  chan<- extsort.SortType
 	tiffTiles        <-chan extsort.SortType
 	tiffTilesSortErr <-chan error
 	uniform          map[uint32]tiffTile
 	tempFile         *os.File
+	tempFileSize     uint64
 	dataSize         uint64
+	zoom             uint8
+
+	// For each zoom level, tileOffsets is the position of the TileOffset
+	// relative to the start of the temporary file. In the final output,
+	// we need to group together the tiles from the same zoom level.
+	tileOffsets    [][]uint32
+	tileByteCounts [][]uint32
 }
 
 func NewRasterWriter(path string, zoom uint8) (*RasterWriter, error) {
@@ -78,18 +87,24 @@ func NewRasterWriter(path string, zoom uint8) (*RasterWriter, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	inChan := make(chan extsort.SortType, 10000)
 	config := extsort.DefaultConfig()
 	config.NumWorkers = runtime.NumCPU()
 	sorter, outChan, errChan := extsort.New(inChan, tiffTileFromBytes, tiffTileLess, config)
 	go sorter.Sort(context.Background())
-	return &RasterWriter{
+	r := &RasterWriter{
+		path:             path,
 		tempFile:         tempFile,
 		tiffTilesToSort:  inChan,
 		tiffTiles:        outChan,
 		tiffTilesSortErr: errChan,
 		uniform:          make(map[uint32]tiffTile, 16),
-	}, nil
+		zoom:             zoom,
+		tileOffsets:      make([][]uint32, zoom+1),
+		tileByteCounts:   make([][]uint32, zoom+1),
+	}
+	return r, nil
 }
 
 func (w *RasterWriter) Write(r *Raster) error {
@@ -175,8 +190,16 @@ func (w *RasterWriter) compress(tile TileKey, pixels []float32) (offset uint64, 
 		return 0, 0, err
 	}
 
-	offset = w.dataSize
-	w.dataSize += uint64(len)
+	offset = w.tempFileSize
+	w.tempFileSize += uint64(len)
+
+	zoom, _, _ := tile.ZoomXY()
+	if w.tileOffsets[zoom] == nil {
+		w.tileOffsets[zoom] = make([]uint32, 1<<(2*zoom))
+		w.tileByteCounts[zoom] = make([]uint32, 1<<(2*zoom))
+	}
+	// TODO: Store offset/len into tileOffset/tileByteCount.
+
 	return offset, uint32(len), nil
 }
 
@@ -189,6 +212,23 @@ func (w *RasterWriter) Close() error {
 		return err
 	}
 
+	out, err := os.Create(w.path + ".tmp")
+	if err != nil {
+		return err
+	}
+	if err := w.writeTiff(out); err != nil {
+		return err
+	}
+	if err := out.Sync(); err != nil {
+		return err
+	}
+	if err := out.Close(); err != nil {
+		return err
+	}
+	if err := os.Rename(w.path+".tmp", w.path); err != nil {
+		return err
+	}
+
 	// Delete the temporary file for compressed data of TIFF tiles.
 	tempFileName := w.tempFile.Name()
 	if err := w.tempFile.Close(); err != nil {
@@ -198,7 +238,199 @@ func (w *RasterWriter) Close() error {
 		return err
 	}
 
-	fmt.Printf("Total data size: %d\n", w.dataSize)
+	return nil
+}
+
+func (w *RasterWriter) writeTiff(out *os.File) error {
+	// Magic header for little-endian TIFF files, followed by offset
+	// to first Image File Directory in the file.
+	magic := []byte{'I', 'I', 42, 0, 8, 0, 0, 0}
+	if _, err := out.Write(magic); err != nil {
+		return err
+	}
+	fileSize := uint32(len(magic))
+
+	const (
+		imageWidth       = 256
+		imageHeight      = 257
+		bitsPerSample    = 258
+		compression      = 259
+		imageDescription = 270
+		samplesPerPixel  = 277
+		software         = 305
+		tileWidth        = 322
+		tileLength       = 323
+		tileOffsets      = 324
+		tileByteCounts   = 325
+		sampleFormat     = 339
+
+		asciiFormat = 2
+		shortFormat = 3
+		longFormat  = 4
+
+		flateCompression = 8
+	)
+
+	// TODO: To emit overviews, make this a loop over multiple zooms,
+	// and patch the IFD offsets so they form a linked list in the TIFF file.
+	// In a cloud-optimized GeoTIFF file, the IFDs have to be sorted
+	// from deepest to coarsest zoom level.
+	numTiles := uint32(1 << (w.zoom * 2))
+	ifd := []struct {
+		tag uint16
+		val uint32
+	}{
+		{imageWidth, 1 << (w.zoom + 8)},
+		{imageHeight, 1 << (w.zoom + 8)},
+		{bitsPerSample, 32},
+		{compression, flateCompression},
+		{imageDescription, 0},
+		{samplesPerPixel, 1},
+		{software, 0},
+		{tileWidth, 256},
+		{tileLength, 256},
+		{tileOffsets, 0},
+		{tileByteCounts, 0},
+		{sampleFormat, 3}, // 3 = IEEE floating point data, TIFF spec p.80
+	}
+
+	// Position of extra data that does not fit inline in Image File Directory,
+	// relative to start of TIFF file.
+	tileOffsetsPos := fileSize + uint32(len(ifd)*12+6)
+	tileByteCountsPos := tileOffsetsPos + uint32(numTiles*4)
+	extraPos := tileByteCountsPos + uint32(numTiles*4)
+
+	var buf, extraBuf bytes.Buffer
+	if err := binary.Write(&buf, binary.LittleEndian, uint16(len(ifd))); err != nil {
+		return err
+	}
+
+	lastTag := uint16(0)
+	for _, e := range ifd {
+		// Sanity check that our tags appear in the Image File Directory
+		// in increasing order, as required by the TIFF specification.
+		if e.tag <= lastTag {
+			panic("TIFF tags must be in increasing order")
+		}
+		lastTag = e.tag
+
+		if err := binary.Write(&buf, binary.LittleEndian, e.tag); err != nil {
+			return err
+		}
+		var typ uint16
+		var count, value uint32
+		switch e.tag {
+		case imageDescription:
+			s := []byte("OpenStreetMap view density, in weekly user views per km2\u0000")
+			typ, count, value = asciiFormat, uint32(len(s)), extraPos+uint32(extraBuf.Len())
+			extraBuf.Write(s)
+
+		case software:
+			s := []byte("TileRank\u0000")
+			typ, count, value = asciiFormat, uint32(len(s)), extraPos+uint32(extraBuf.Len())
+			extraBuf.Write(s)
+
+		case tileOffsets:
+			typ, count, value = longFormat, numTiles, tileOffsetsPos
+
+		case tileByteCounts:
+			typ, count, value = longFormat, numTiles, tileByteCountsPos
+
+		default:
+			typ, count, value = longFormat, uint32(1), e.val
+			if e.val <= 0xffff {
+				typ = shortFormat
+			}
+		}
+		if err := binary.Write(&buf, binary.LittleEndian, typ); err != nil {
+			return err
+		}
+		if err := binary.Write(&buf, binary.LittleEndian, count); err != nil {
+			return err
+		}
+		if err := binary.Write(&buf, binary.LittleEndian, value); err != nil {
+			return err
+		}
+	}
+
+	nextIFD := uint32(0)
+	if err := binary.Write(&buf, binary.LittleEndian, nextIFD); err != nil {
+		return err
+	}
+
+	if _, err := out.Write(buf.Bytes()); err != nil {
+		return err
+	}
+	fileSize += uint32(buf.Len())
+
+	// Reserve space for tileOffsets. We will overwrite tileOffsets
+	// later, when writing out the actual data, since only then we’ll
+	// know the actual offsets in the file.
+	if fileSize != tileOffsetsPos {
+		panic("fileSize != tileOffsetsPos")
+	}
+	numRows := 1 << w.zoom
+	emptyRow := make([]byte, numRows*4)
+	for y := 0; y < numRows; y++ {
+		if _, err := out.Write(emptyRow); err != nil {
+			return err
+		}
+	}
+	fileSize += uint32(numTiles * 4)
+
+	// Write tileByteCounts. Unlike tileOffsets, we already know the
+	// final byte counts because the tile size is the same as in the
+	// temporary file.
+	if fileSize != tileByteCountsPos {
+		panic("fileSize != tileByteCountsPos")
+	}
+	if err := binary.Write(out, binary.LittleEndian, w.tileByteCounts[w.zoom]); err != nil {
+		return err
+	}
+	fileSize += uint32(len(w.tileByteCounts[w.zoom]) * 4)
+
+	// Add padding to extraBuf so its length is a multiple of four.
+	if pad := (4 - extraBuf.Len()%4) % 4; pad > 0 {
+		if _, err := extraBuf.Write([]byte{0, 0, 0, 0}[:pad]); err != nil {
+			return err
+		}
+	}
+	if fileSize != extraPos {
+		panic("fileSize != extraPos")
+	}
+	if _, err := extraBuf.WriteTo(out); err != nil {
+		return err
+	}
+	fileSize += uint32(extraBuf.Len())
+
+	alreadyWritten := make(map[uint32]uint32, numTiles)
+	finalTileOffsets := make([]uint32, numTiles)
+	for tile := uint32(0); tile < numTiles; tile++ {
+		tileOffset := w.tileOffsets[w.zoom][tile]
+		if off, exists := alreadyWritten[tileOffset]; !exists {
+			tileData := make([]byte, w.tileByteCounts[w.zoom][tile])
+			if _, err := w.tempFile.ReadAt(tileData, int64(tileOffset)); err != nil {
+				return err
+			}
+			off = fileSize
+			finalTileOffsets[tile] = off
+			alreadyWritten[tileOffset] = off
+			if _, err := out.Write(tileData); err != nil {
+				return err
+			}
+			fileSize += uint32(len(tileData))
+		} else {
+			finalTileOffsets[tile] = off
+		}
+	}
+
+	if _, err := out.Seek(int64(tileOffsetsPos), 0); err != nil {
+		return err
+	}
+	if err := binary.Write(out, binary.LittleEndian, finalTileOffsets); err != nil {
+		return err
+	}
+
 	return nil
 }
 
