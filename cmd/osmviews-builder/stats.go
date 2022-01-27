c56dbf6fffde505e183b02bdb6087d978ceecb4d
Sascha Brawer <sascha@brawer.ch>
2022-01-27 17:44:38 +0100

    Pick random samples for shared tiles

--- cmd/osmviews-builder/stats.go
+++ cmd/osmviews-builder/stats.go
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2022 Sascha Brawer <sascha@brawer.ch>
+// SPDX-License-Identifier: MIT
+
 package main
 
 import (
@@ -8,6 +11,7 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"math/rand"
 	"os"
 	"sort"
 	"time"
@@ -226,9 +230,90 @@ type bucket struct {
 	sample bucketSample
 }
 
+type TileIndex int
+
+// SharedTile keeps information about a tile is used more than once.
+// In our GeoTIFF, 93.1% of all tile offsets point to a shared tile.
+// Usually these are patches of oceans or deserts into which no map user
+// ever zooms deeply.
+type SharedTile struct {
+	UseCount    int         // Total number of tiles sharing this data.
+	SampleTiles []TileIndex // A random sample of tiles that share this data.
+}
+
+type SharedTiles map[uint32]*SharedTile
+
+func findSharedTiles(tileOffsets []uint32) SharedTiles {
+	shared := make(SharedTiles, 20)     // 16 for GeoTIFF of 2022-01-24
+	uses := make(map[uint32]int, 80000) // 72138 for TIFF of 2022-01-24
+	for _, off := range tileOffsets {
+		uses[off] += 1
+	}
+
+	for off, n := range uses {
+		if n > 1 {
+			r := SharedTile{UseCount: n, SampleTiles: make([]TileIndex, 2000)}
+			for i := 0; i < len(r.SampleTiles); i++ {
+				r.SampleTiles[i] = -1
+			}
+			shared[off] = &r
+		}
+	}
+
+	stride := 1 << (math.Ilogb(float64(len(tileOffsets))) / 2)
+	for _, y := range rand.Perm(stride) {
+		for x := 0; x < stride; x++ {
+			tile := TileIndex(y*stride + x)
+			off := tileOffsets[tile]
+			if r, ok := shared[off]; ok {
+				key := int(tile) % len(r.SampleTiles)
+				if r.SampleTiles[key] < 0 || rand.Intn(50) == 0 {
+					r.SampleTiles[key] = tile
+				}
+			}
+		}
+	}
+
+	// If any slots are left unused, remove them.
+	for _, st := range shared {
+		j := 0
+		for i := 0; i < len(st.SampleTiles); i++ {
+			if st.SampleTiles[i] >= 0 {
+				st.SampleTiles[j] = st.SampleTiles[i]
+				j++
+			}
+		}
+		st.SampleTiles = st.SampleTiles[0:j]
+	}
+
+	return shared
+}
+
+type histogram struct {
+	buckets map[uint64]bucket
+}
+
+func newHistogram() *histogram {
+	h := &histogram{}
+	h.buckets = make(map[uint64]bucket, 250000) // 210037 for 2022-01-24 data
+	return h
+}
+
+func (h *histogram) process(data []float32, uses int) {
+}
+
 func buildHistogram(t *TiffReader) ([]bucket, error) {
-	tileUses := make(map[uint32]int, 2000) // 1647 for TIFF of Jan 24, 2022
-	tile := make(map[uint32]int, 2000)
+	sharedTiles := findSharedTiles(t.tileOffsets)
+
+	sharedTileSamples := make(map[TileIndex]bool, 1000) // 894 for 2022-01-24
+	for _, t := range sharedTiles {
+		for _, sample := range t.SampleTiles {
+			sharedTileSamples[sample] = true
+		}
+	}
+
+	tileUses := make(map[uint32]int, 80000) // 72138 for TIFF of Jan 24, 2022
+	tile := make(map[uint32]int, 80000)
 	for i, off := range t.tileOffsets {
 		tile[off] = i
 		tileUses[off] += 1
@@ -239,19 +324,49 @@ func buildHistogram(t *TiffReader) ([]bucket, error) {
 	tileStride := int((t.imageWidth + t.tileWidth - 1) / t.tileWidth)
 	zoom := uint8(math.Ilogb(float64(t.imageWidth)))
 	hist := make(map[int64]bucket, 250000) // 210037 for TIFF of Jan 24, 2022
+	//hist2 := newHistogram()
 	var n int
 
-	for i, ti := range tile {
+	dc1 := gg.NewContext(tileStride, tileStride)
+	dc1.SetRGB(1, 1, 1)
+	dc1.Clear()
+	dc1.SetRGB(0.8, 0.8, 1)
+	for tii, off := range t.tileOffsets {
+		if sharedTiles[off] != nil {
+			dc1.SetPixel(tii%tileStride, tii/tileStride)
+		}
+	}
+
+	dc1.SetRGB(0, 0.4, 1)
+	for tii, off := range t.tileOffsets {
+		ti := TileIndex(tii)
+		shared := sharedTiles[off]
+		if shared != nil && !sharedTileSamples[ti] {
+			continue
+		}
+		tileX, tileY := tii%tileStride, tii/tileStride
+		if shared != nil {
+			dc1.DrawCircle(float64(tileX), float64(tileY), 2.0)
+			dc1.Fill()
+		}
+	}
+
+	if err := dc1.SavePNG("debug.png"); err != nil {
+		return nil, err
+	}
+
+	for off, ti := range tile {
+		continue
 		if err := t.readTile(ti, data); err != nil {
 			return nil, err
 		}
-		uses := int64(tileUses[i])
+		uses := int64(tileUses[off])
 		tileX, tileY := ti%tileStride, ti/tileStride
 		for px, value := range data {
-			key := int64(value+0.5) + 100
+			key := int64(value + 0.5) //+ 100
 			// Collect more samples for small values.
 			if value < 100 {
-				key = int64(value*100 + 0.5)
+				//key = int64(value*100 + 0.5)
 			}
 			if h, present := hist[key]; present {
 				// frequent code path
