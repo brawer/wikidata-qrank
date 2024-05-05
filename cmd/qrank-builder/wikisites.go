bca78e856b7de950067b6ed83851d9d873aae1a1
Sascha Brawer <sascha@brawer.ch>
2024-05-05 12:07:35 +0200

    Add helper for finding Wiki sites with their domain and last-dumped date

---
+++ cmd/qrank-builder/wikisites.go
@@ -0,0 +1,101 @@
+// SPDX-FileCopyrightText: 2024 Sascha Brawer <sascha@brawer.ch>
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"compress/gzip"
+	"fmt"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+	"time"
+)
+
+// WikiSite keeps what we know about a Wikimedia site such as en.wikipedia.org.
+type WikiSite struct {
+	Key        string    // Wikimedia key, such as "enwiki"
+	Domain     string    // Internet domain, such as "en.wikipedia.org"
+	LastDumped time.Time // Date of last complete database dump
+}
+
+func ReadWikiSites(dumps string) (*map[string]WikiSite, error) {
+	dirContent, err := os.ReadDir(dumps)
+	if err != nil {
+		return nil, err
+	}
+	dumpDirs := make(map[string]os.DirEntry, len(dirContent))
+	for _, d := range dirContent {
+		dumpDirs[d.Name()] = d
+	}
+
+	sites := make(map[string]WikiSite, 400)
+	f, err := os.Open(filepath.Join(
+		dumps, "metawiki", "latest/metawiki-latest-sites.sql.gz",
+	))
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	gz, err := gzip.NewReader(f)
+	if err != nil {
+		return nil, err
+	}
+	defer gz.Close()
+
+	reader, err := NewSQLReader(gz)
+	if err != nil {
+		return nil, err
+	}
+
+	columns := reader.Columns()
+	globalKeyCol := slices.Index(columns, "site_global_key")
+	domainCol := slices.Index(columns, "site_domain")
+	for {
+		row, err := reader.Read()
+		if row == nil {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		site := WikiSite{
+			Key:    row[globalKeyCol],
+			Domain: decodeDomain(row[domainCol]),
+		}
+		if dirent, ok := dumpDirs[site.Key]; !ok || !dirent.IsDir() {
+			continue
+		}
+
+		for _, f := range []string{"page_props.sql.gz"} {
+			latestFile := fmt.Sprintf("%s-latest-%s", site.Key, f)
+			latestPath := filepath.Join(dumps, site.Key, "latest", latestFile)
+			if latest, err := filepath.EvalSymlinks(latestPath); err == nil {
+				dir, _ := filepath.Split(latest)
+				_, version := filepath.Split(filepath.Dir(dir))
+				if dumped, err := time.Parse("20060102", version); err == nil {
+					if site.LastDumped.IsZero() || dumped.Before(site.LastDumped) {
+						site.LastDumped = dumped
+
+					}
+				}
+			}
+		}
+
+		sites[site.Key] = site
+	}
+
+	return &sites, nil
+}
+
+func decodeDomain(s string) string {
+	s = strings.TrimSuffix(s, ".")
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
---
+++ cmd/qrank-builder/wikisites_test.go
@@ -0,0 +1,43 @@
+// SPDX-FileCopyrightText: 2024 Sascha Brawer <sascha@brawer.ch>
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+)
+
+func TestReadWikiSites(t *testing.T) {
+	sites, err := ReadWikiSites(filepath.Join("testdata", "dumps"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	tests := []struct{ key, domain, lastDumped string }{
+		{"metawiki", "meta.wikimedia.org", ""},
+		{"rmwiki", "rm.wikipedia.org", "2024-03-01"},
+	}
+	for _, tc := range tests {
+		site := (*sites)[tc.key]
+		if site.Domain != tc.domain {
+			t.Errorf(`got "%s", want "%s", for sites["%s"].Domain`, site.Domain, tc.domain, tc.key)
+		}
+		lastDumped := site.LastDumped.Format(time.DateOnly)
+		if site.LastDumped.IsZero() {
+			lastDumped = ""
+		}
+		if lastDumped != tc.lastDumped {
+			t.Errorf(`got %s, want %s, for sites["%s"].LastDumped`, lastDumped, tc.lastDumped, tc.key)
+		}
+	}
+}
+
+func TestReadWikiSites_BadPath(t *testing.T) {
+	_, err := ReadWikiSites(filepath.Join("testdata", "no-such-dir"))
+	if !os.IsNotExist(err) {
+		t.Errorf("want os.NotExists, got %v", err)
+	}
+}
